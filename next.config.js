b395b910f59714af0439e05a8fb7e627d0219530
bee <vogiabao292@gmail.com>
2023-04-19 19:32:52 +0700

    baovo: next and i18n config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const { i18n } = require("./next-i18next.config");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n,
   reactStrictMode: true,
-}
+  swcMinify: true,
+  output: "standalone",
+  env: {
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

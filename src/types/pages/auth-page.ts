2589d993f2108e909790f624ea6e55a4fc05dd89
bee <vogiabao292@gmail.com>
2023-04-30 16:43:03 +0700

    baovo: implement auth

---
+++ src/types/pages/auth-page.ts
@@ -0,0 +1,13 @@
+export type LoginForm = {
+    email: string;
+    password: string;
+}
+
+export type RegisterForm = {
+    email: string;
+    password: string;
+    name: string;
+    dob: string;
+    gender: string;
+    confirmPassword: string
+}

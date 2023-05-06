e23b8ccfc1c83382c9f0ab9181834ca5f1a0a5f0
GiangNguyenn <giang.nguyen@skedulo.com>
2023-05-06 16:21:03 +0700

    add setting layout

--- src/hoc/withSettings.tsx
+++ src/hoc/withSettings.tsx
@@ -4,17 +4,13 @@ import Footer from "@/shared/footer";
 import Header from "@/shared/header";
 
 export default function withSettings<P extends object>(
-  SideBar: React.ComponentType<P>,
-  Content: React.ComponentType<P>
+  SideBar: React.ComponentType<P>
 ): React.FC<P> {
   const WithSettings: React.FC<P> = (props: P) => {
     return (
       <div className="flex h-screen flex-col">
         <div className="flex flex-1">
           <SideBar {...props} />
-          <main className="flex-1 bg-white">
-            <Content {...props} />
-          </main>
         </div>
       </div>
     );

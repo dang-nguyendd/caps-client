2cd85a276b0b41d9a1bdae18526c432da0028eb4
s3822042 <s3822042@rmit.edu.vn>
2023-05-11 00:34:39 +0700

    update

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,14 +1,8 @@
-import LineChart from "@/components/chart-analysist/line-chart";
-
 const Component = () => {
   return (
     <div className="flex flex-wrap">
-      <div className="mb-12 w-full px-4 xl:mb-0 xl:w-8/12">
-        <LineChart />
-      </div>
-      <div className="w-full px-4 xl:w-4/12">
-        <LineChart />
-      </div>
+      <div className="mb-12 w-full px-4 xl:mb-0 xl:w-8/12">{/*  Chart 1*/}</div>
+      <div className="w-full px-4 xl:w-4/12">{/*  Chart 2*/}</div>
     </div>
   );
 };

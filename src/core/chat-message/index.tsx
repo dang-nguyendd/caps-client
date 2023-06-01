5c33a6e814da817d540ae8474020497265c523a1
s3822042 <s3822042@rmit.edu.vn>
2023-06-02 00:53:55 +0700

    update

--- src/core/chat-message/index.tsx
+++ src/core/chat-message/index.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { IChatMessageProps } from "@/core/chat-message/type";
 
 const Component = React.memo((props: IChatMessageProps) => {
   const { conservationId, content, senderType } = props;
+  const [isSpeaking, setIsSpeaking] = useState(false);
 
   const containerClasses = {
     chatbot: "justify-start",
@@ -18,6 +19,24 @@ const Component = React.memo((props: IChatMessageProps) => {
   const containerClass = containerClasses[senderType];
   const messageClass = messageClasses[senderType];
 
+  const _speakMessage = () => {
+    const utterance = new SpeechSynthesisUtterance(content);
+
+    utterance.voice = speechSynthesis.getVoices()[0];
+
+    speechSynthesis.speak(utterance);
+
+    utterance.onend = () => {
+      setIsSpeaking(false);
+    };
+  };
+
+  useEffect(() => {
+    if (isSpeaking) {
+      _speakMessage();
+    }
+  }, [isSpeaking]);
+
   return (
     <div
       className={`mt-5 grid grid-flow-row gap-2 text-sm text-gray-700  ${containerClass}`}
@@ -29,6 +48,7 @@ const Component = React.memo((props: IChatMessageProps) => {
         >
           {content}
         </p>
+        <button onClick={() => setIsSpeaking(true)}>Speak</button>
       </div>
     </div>
   );

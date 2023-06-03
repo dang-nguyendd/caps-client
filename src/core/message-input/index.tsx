c3dd815faf32b6df454354508e5996515d143b66
s3822042 <s3822042@rmit.edu.vn>
2023-06-03 15:33:57 +0700

    update

--- src/core/message-input/index.tsx
+++ src/core/message-input/index.tsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
-import { IconSend, IconHeartbeat, IconMicrophone } from "@tabler/icons-react";
-import annyang from "annyang";
+import { IconSend, IconHeartbeat } from "@tabler/icons-react";
 
 import { IMessageInputProps } from "@/core/message-input/type";
 import HealthStatusPopupModal from "@/shared/health-status-popup-modal";
@@ -15,18 +14,6 @@ const Component: React.FC<IMessageInputProps> = ({
 }) => {
   const [isHealthStatusModalOpen, setIsHealthStatusPopupModalOpen] =
     useState<boolean>(false);
-  const [transcript, setTranscript] = useState<string>("");
-
-  // const handleRecording = () => {
-  //   if (!annyang.isListening()) {
-  //     annyang.start({ autoRestart: false, continuous: true });
-  //   }
-  // };
-  //
-  // const handleSpeechResult = (transcripts: string[]) => {
-  //   setTranscript(transcripts[0]);
-  //   onValueChange(transcripts[0]);
-  // };
 
   const handleSpeechError = (error: any) => {
     console.error("Speech recognition error:", error);
@@ -34,39 +21,13 @@ const Component: React.FC<IMessageInputProps> = ({
 
   const handleSubmit = () => {};
 
-  // useEffect(() => {
-  //   const commands = {
-  //     speak: handleSpeechResult,
-  //     nói: handleSpeechResult,
-  //   };
-  //
-  //   annyang.addCommands(commands);
-  //   annyang.addCallback("error", handleSpeechError);
-  //
-  //   return () => {
-  //     annyang.abort();
-  //     annyang.removeCommands(Object.keys(commands));
-  //     annyang.removeCallback("error", handleSpeechError);
-  //   };
-  // }, []);
-
   return (
     <div className="flex-none">
       <div className="flex flex-row items-center p-4">
-        <button
-          className="mx-2 flex h-6 w-6 shrink-0 text-blue hover:text-blue focus:outline-none"
-          // onClick={handleRecording}
-        >
-          {true ? (
-            <div className="h-4 w-4 animate-spin rounded-full border-t-2 border-neutral-800 opacity-60 dark:border-neutral-100"></div>
-          ) : (
-            <IconMicrophone color="white" />
-          )}
-        </button>
         <input
           data-tour={dataTourTwo}
           className="w-full rounded-full border border-gray-800 bg-gray-800 py-2 pl-3 pr-10 text-gray-200 transition duration-300 ease-in focus:border-gray-700 focus:bg-gray-900 focus:shadow-md focus:outline-none"
-          value={transcript || message}
+          value={message}
           onChange={(e) => onValueChange(e.target.value)}
           onKeyDown={handleKeyDown}
           placeholder="Ask anything (Shift-Enter for new line, Enter to send)"
--- src/shared/popover/constant.ts
+++
@@ -1,4 +0,0 @@
-export const exportOptions = [
-  { label: "Export JSON", href: "/export-json" },
-  { label: "Export Markdown", href: "/export-markdown" },
-];
